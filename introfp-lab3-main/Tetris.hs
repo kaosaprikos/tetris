86e8a6126cc2944722ad5eae74162d1062407041
AlbaMoriWallin <93259474+AlbaMoriWallin@users.noreply.github.com>
2025-10-23 16:29:52 +0200

    Update Tetris.hs

--- introfp-lab3-main/Tetris.hs
+++ introfp-lab3-main/Tetris.hs
@@ -252,6 +252,462 @@ clearLines (Shape rows) =
   in 
     (clearedCount, Shape newRows)  -- returns how many rows were removed and updates the well
 
+
+
+    {- |
+Module      : Simplify
+Description : Skeleton for Lab 4: simplifying polynomials.
+Copyright   : (c) TDA555/DIT441, Introduction to Functional Programming
+License     : BSD
+Maintainer  : alexg@chalmers.se
+Stability   : experimental
+
+Authors     : <Alba Mori Wallin, Amanda Andino Juarez, Harsimrat Kour>
+Lab group   : <group 22>
+-}
+
+module Simplify where
+
+import Parser
+import Poly
+import Test.QuickCheck
+import Data.Maybe
+import System.Directory (doesFileExist)
+import System.Random (randomRIO)
+import Test.QuickCheck (generate, vectorOf, choose)
+import Control.Monad (forever)
+
+-- Use the following simple data type for binary operators
+data BinOp = AddOp | MulOp deriving Eq
+
+--------------------------------------------------------------------------------
+-- * A1
+
+{-PROMPT
+
+Ask the coding assistant to define a data type Expr to represent arithmetic expressions. 
+The data type should be able to represent, integers, expressions built using addition and multiplication, 
+and integer powers of a single variable e.g. x2, x9, x10 etc. All integers 
+here should make use of Haskell’s Int type. For binary operators it should use a single 
+constructor and you must make use of the helper data type BinOp:
+
+data BinOp = AddOp | MulOp
+It should not be able to represent exponential expressions which are anything except x raised 
+to some integer power, so for example you should not be able to represent x(1 + 1), 2x, 23, 
+etc. Similarly, it should not be able to represent expressions with more than one variable, 
+so you should not be able to represent x + y.-}
+
+
+-- Expression type
+data Expr
+  = Const Int                -- Integer constant
+  | VarPow Int               -- x raised to a non-negative integer power (e.g., x^2, x^9)
+  | BinExpr BinOp Expr Expr  -- Addition or multiplication of two expressions
+  deriving (Eq)
+
+--------------------------------------------------------------------------------
+-- * A2
+
+
+prop_expr :: Expr -> Bool
+prop_expr (Const _)         = True
+prop_expr (VarPow n)        = n >= 0
+prop_expr (BinExpr _ e1 e2) = prop_expr e1 && prop_expr e2
+
+exp8 = VarPow 8
+exp9 = VarPow (-6)
+exp10 = BinExpr AddOp (VarPow 8) (VarPow (-9))
+exp11 = BinExpr MulOp (VarPow 10) (VarPow 3)
+
+
+
+
+--------------------------------------------------------------------------------
+-- * A3
+
+{- PROMPT
+
+Use the coding assistant to make Expr an instance of Show, which makes sure that 
+only brackets are added where they are needed. Use Haskell notation for exponents e.g. x^2. 
+The instance should show x1 as just x, but otherwise it should not simplify your expressions 
+in any way before it shows them.-}
+
+instance Show BinOp where
+  show AddOp = "+"
+  show MulOp = "*"
+
+instance Show Expr where
+  show = showExpr
+
+-- Helper to determine precedence
+precedence :: BinOp -> Int
+precedence AddOp = 1
+precedence MulOp = 2
+
+-- Show expression with minimal parentheses
+showExpr :: Expr -> String
+showExpr = go 0
+  where
+    go _ (Const n) = show n
+    go _ (VarPow 1) = "x"
+    go _ (VarPow n) = "x^" ++ show n
+    go p (BinExpr op e1 e2) =
+      let opPrec = precedence op
+          left = go opPrec e1
+          right = go (opPrec + 1) e2
+          inner = left ++ " " ++ show op ++ " " ++ right
+      in if opPrec < p then "(" ++ inner ++ ")" else inner
+
+exp1 = Const 5
+exp2 = VarPow 1
+exp3 = VarPow 8
+exp4 = BinExpr AddOp exp1 exp2
+exp5 = BinExpr MulOp exp4 exp1
+exp6 = BinExpr AddOp exp5 exp4
+exp7 = BinExpr MulOp exp5 exp6
+ 
+--------------------------------------------------------------------------------
+-- * A4
+
+{- PROMPT
+
+Let the coding assistant make Expr an instance of Arbitrary and check the data 
+type invariant that you defined in A2 using QuickCheck. 
+You should make sure that the generated expressions are not too big. 
+Hint: we discussed in a lecture how to write a generator that takes the size 
+of the to be generated expression as an argument. Pass this information on to 
+the coding assistant.-}
+
+-- Must come before genExpr
+instance Arbitrary BinOp where
+  arbitrary = elements [AddOp, MulOp]
+
+
+instance Arbitrary Expr where
+  arbitrary = sized genExpr
+
+genExpr :: Int -> Gen Expr
+genExpr 0 = oneof [Const <$> genInt, VarPow <$> choose (0, 5)]
+genExpr n = frequency
+  [ (1, Const <$> genInt)
+  , (1, VarPow <$> choose (0, 5))
+  , (2, do
+        op <- arbitrary  -- This now works because BinOp has an Arbitrary instance
+        let n' = n `div` 2
+        e1 <- genExpr n'
+        e2 <- genExpr n'
+        return $ BinExpr op e1 e2)
+  ]
+
+
+-- Helper to avoid ambiguity
+genInt :: Gen Int
+genInt = Test.QuickCheck.arbitrary
+
+
+--------------------------------------------------------------------------------
+-- * A5
+
+
+{- PROMPT 
+
+Instruct the coding assistant to define a function eval :: Int -> Expr -> Int 
+that takes a value for x and an expression and evaluates it.-}
+
+
+-- Evaluate an expression given a value for x
+eval :: Int -> Expr -> Int
+eval _ (Const n) = n
+eval x (VarPow n) = x ^ n
+eval x (BinExpr op e1 e2) =
+  let v1 = eval x e1
+      v2 = eval x e2
+  in case op of
+       AddOp -> v1 + v2
+       MulOp -> v1 * v2 
+
+-- Write properties!
+
+prop_eval_add :: Int -> Expr -> Expr -> Bool
+prop_eval_add x e1 e2 = (eval x e1) + (eval x e2) == eval x (BinExpr AddOp e1 e2)
+
+prop_eval_mul :: Int -> Expr -> Expr -> Bool
+prop_eval_mul x e1 e2 = (eval x e1) * (eval x e2) == eval x (BinExpr MulOp e1 e2)
+--------------------------------------------------------------------------------
+-- * A6
+
+{- PROMT
+
+Ask the coding assistant to define exprToPoly :: Expr -> Poly that converts an expression into a polynomial. -}
+
+
+exprToPoly :: Expr -> Poly
+exprToPoly (Const n) = fromInteger (toInteger n)
+exprToPoly (VarPow k) = fromList (replicate k 0 ++ [1])
+exprToPoly (BinExpr AddOp e1 e2) = exprToPoly e1 + exprToPoly e2
+exprToPoly (BinExpr MulOp e1 e2) = exprToPoly e1 * exprToPoly e2
+
+-- Define (and check) prop_exprToPoly, which checks that evaluating the
+-- polynomial you get from exprToPoly gives the same answer as evaluating
+-- the expression.
+
+prop_exprToPoly :: Int -> Expr -> Bool
+prop_exprToPoly x expr = eval x expr == evalPoly x (exprToPoly expr)
+
+--------------------------------------------------------------------------------
+-- * A7
+-- Now define the function going in the other direction.
+
+{- PROMT
+Continue with the generation of a function going in the other direction, 
+polyToExpr :: Poly -> Expr. Instruct the coding assistant to use “smart constructors” 
+to ensure that you don’t introduce “junk” like multiplication by 1 in your result. 
+We covered such smart constructors in week 6.-}
+
+-- Smart constructor for constants
+mkConst :: Int -> Maybe Expr
+mkConst 0 = Nothing
+mkConst n = Just (Const n)
+
+-- Smart constructor for variable powers
+mkVarPow :: Int -> Maybe Expr
+mkVarPow 0 = Nothing
+mkVarPow 1 = Just (VarPow 1)
+mkVarPow n = Just (VarPow n)
+
+-- Smart constructor for multiplication
+mkMul :: Expr -> Expr -> Expr
+mkMul (Const 1) e = e
+mkMul e (Const 1) = e
+mkMul e1 e2       = BinExpr MulOp e1 e2
+
+-- Smart constructor for addition
+mkAdd :: Expr -> Expr -> Expr
+mkAdd (Const 0) e = e
+mkAdd e (Const 0) = e
+mkAdd e1 e2       = BinExpr AddOp e1 e2
+
+-- ...existing code...
+polyToExpr :: Poly -> Expr
+polyToExpr p =
+  case catMaybes terms of
+    []     -> Const 0
+    (t:ts) -> foldl mkAddExpr t ts
+  where
+    -- toList returns coefficients most-significant-first, so reverse to get
+    -- little-endian [c0,c1,c2,...] where index = power
+    coeffs = reverse (toList p)
+    terms  = [term c pow | (pow, c) <- zip [0..] coeffs]
+
+    term 0 _ = Nothing
+    term c 0 = mkConst c
+    term c 1 = Just $ mkMul (Const c) (VarPow 1)
+    term c k = Just $ mkMul (Const c) (VarPow k)
+
+    mkAddExpr = mkAdd
+
+
+-- Write (and check) a quickCheck property for this function similar to
+-- question 6. 
+
+prop_polyToExpr :: Int -> Poly -> Bool
+prop_polyToExpr x pol = eval x (polyToExpr pol) == evalPoly x pol
+--------------------------------------------------------------------------------
+-- * A8
+
+{- PROMPT
+Let the coding assistant write a function simplify :: Expr -> Expr that simplifies 
+an expression by converting it to a polynomial and back again (this is easy)-}
+
+simplify :: Expr -> Expr
+simplify = polyToExpr . exprToPoly
+
+--------------------------------------------------------------------------------
+-- * A9
+
+{- PROMPT
+The idea with simplify is that it should end you up with a simplified expression (of course). 
+But this depends exactly how the function polyToExpr is defined (in A7)-}
+
+prop_noJunk :: Expr -> Bool
+prop_noJunk = noJunk . simplify
+
+-- Recursive check for junk patterns
+noJunk :: Expr -> Bool
+noJunk (Const _) = True
+noJunk (VarPow 0) = False  -- x^0 is junk
+noJunk (VarPow _) = True
+noJunk (BinExpr AddOp (Const 0) _) = False
+noJunk (BinExpr AddOp _ (Const 0)) = False
+noJunk (BinExpr AddOp (Const _) (Const _)) = False
+noJunk (BinExpr MulOp (Const 0) _) = False
+noJunk (BinExpr MulOp _ (Const 0)) = False
+noJunk (BinExpr MulOp (Const 1) _) = False
+noJunk (BinExpr MulOp _ (Const 1)) = False
+noJunk (BinExpr MulOp (Const _) (Const _)) = False
+noJunk (BinExpr _ exp1 exp2) = noJunk exp1 && noJunk exp2
+
+
+--------------------------------------------------------------------------------
+-- * A10
+
+{- PROMPT
+
+-We are going to define a function play that lets a user solve polynomial expressions of a particularly difficulty. 
+The difficulty is going to be saved to file, such that a user can resume solving polynomials at the appropriate difficulty. 
+The difficulty is modelled as a natural number and is stored in a file called difficulty.txt. The file should be stored in 
+  the same directory as the source files.
+Let the coding assistant write two IO functions that read respectively write the difficulty:
+
+type Difficulty = Int
+
+readDifficulty :: IO Difficulty
+
+writeDifficulty :: Difficulty -> IO ()-}
+
+
+type Difficulty = Int
+
+diffFile :: FilePath
+diffFile = "difficulty.txt"
+
+readDifficulty :: IO Difficulty
+readDifficulty = do
+  exists <- doesFileExist diffFile
+  if not exists
+    then return 1
+    else do
+      content <- readFile diffFile
+      case reads content of
+        [(n, "")] | n >= 1 -> return n
+        _                  -> return 1
+
+writeDifficulty :: Difficulty -> IO ()
+writeDifficulty n = writeFile diffFile (show n)
+
+
+--------------------------------------------------------------------------------
+-- * A11
+
+
+{- PROMPT
+Continue with the aid of the coding assistant to define the play :: IO () function that
+
+reads the difficulty from file,
+generates a random expression for that difficulty,
+generates a random value for x,
+shows the simplified expression,
+and ask the user to solve it.
+If the guess by the user is as expected: give a nice feedback 
+message and increase the difficulty by one, using the appropriate function from A10. 
+If the guess was wrong, again give feedback to the user and decrease the difficulty by one. 
+After having given feed back you should let the user play again.
+
+The text below shows an example interaction:
+
+ghci> play
+Simplify the following expression with x = 4
+
+13*x^5+15*x^3+57*x^2+15*x
+
+> 132
+No, it should have been 15244.
+
+Simplify the following expression with x = 4
+
+2*x^3
+
+> 128
+Well done!
+
+Simplify the following expression with x = 2
+
+x^3+x^2
+
+>-}
+
+-- Generate a random polynomial based on difficulty
+genPoly :: Difficulty -> IO Poly
+genPoly d = do
+  coeffs <- generate $ vectorOf (d + 1) (choose (-5, 5))
+  return $ fromList coeffs
+
+-- Main play loop
+play :: IO ()
+play = forever $ do
+  difficulty <- readDifficulty
+  poly <- genPoly difficulty
+  let expr = simplify (polyToExpr poly)
+
+  x <- randomRIO (1, 5)
+  putStrLn $ "\nSimplify the following expression with x = " ++ show x ++ "\n"
+  putStrLn $ show expr
+  putStrLn "\n> "
+
+  input <- getLine
+  case reads input of
+    [(guess, "")] -> do
+      let correct = eval x expr
+      if guess == correct
+        then do
+          putStrLn "Well done!"
+          writeDifficulty (difficulty + 1)
+        else do
+          putStrLn $ "No, it should have been " ++ show correct
+
+--------------------------------------------------------------------------------
+-- * A12
+
+{- PROMPT
+
+A parser is a tool (a function) that converts a string to a value of a particular type. 
+For example, a compiler uses a parser to convert the contents of a source code file (a string), 
+to an abstract syntax tree that is used to generate an executable. Ask the coding assistant to define a parser, 
+which must use the Parser.hs module, for the simple arithmetic expression language you have defined. 
+That is, it should write a function that converts a string, 
+which can contain numbers, additions, and multiplications, to a value of type Expr. 
+The function should have the following type signature:
+
+parseExpr :: String -> Maybe Expr
+-}
+
+
+parseExpr :: String -> Maybe Expr
+parseExpr input = parse (trim exprParser) input
+
+-- Expression parser with precedence
+exprParser :: Parse Expr
+exprParser = chainl termParser addOp
+
+termParser :: Parse Expr
+termParser = chainl factorParser mulOp
+
+factorParser :: Parse Expr
+factorParser = parseConst <|> parseVarPow <|> parens exprParser
+
+-- Parse constants like "42"
+parseConst :: Parse Expr
+parseConst = Const <$> int
+
+parseVarPow :: Parse Expr
+parseVarPow = VarPow <$> (char 'x' *> ((char '^' *> int) `opt` 1))
+
+-- Operators
+addOp :: Parse (Expr -> Expr -> Expr)
+addOp = (\_ -> BinExpr AddOp) <$> char '+'
+
+mulOp :: Parse (Expr -> Expr -> Expr)
+mulOp = (\_ -> BinExpr MulOp) <$> char '*'
+
+prop_roundTrip :: Expr -> Bool
+prop_roundTrip e = 
+  case parseExpr (show e) of
+    Just e' -> simplify e == simplify e'
+    Nothing -> False
+
+main :: IO ()
+main = quickCheck prop_roundTrip
+
       
                         
  
